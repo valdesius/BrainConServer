c68e5f724ea883f102589eaa6b11fe150c9ef674
valdesius <v.inc@inbox.ru>
2024-03-07 14:17:41 +0300

    v0.1

---
+++ src/main/java/v/inc/brainconserver/BrainConServerApplication.java
@@ -0,0 +1,12 @@
+package v.inc.brainconserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainConServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BrainConServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/v/inc/brainconserver/BrainConServerApplicationTests.java
@@ -0,0 +1,13 @@
+package v.inc.brainconserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainConServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
